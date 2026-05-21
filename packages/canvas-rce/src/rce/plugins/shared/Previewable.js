3538dabf3b780a5a306f1310c87c001df5cec388
Alex Dombroski <alexdombroski1@gmail.com>
2026-05-21 10:35:18 -0600

    Revert "feat: mark more mime types as previewable"
    This reverts commit 69b8ffadb89b6df13dc93ab0aea183bc731fe8d8.

--- packages/canvas-rce/src/rce/plugins/shared/Previewable.js
+++ packages/canvas-rce/src/rce/plugins/shared/Previewable.js
@@ -40,35 +40,9 @@ export const previewableMimeTypes = [
   'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
   'application/postscript',
   'application/pdf',
-  'application/javascript',
-  'text/javascript',
-  'application/typescript',
-  'text/x-python',
-  'text/x-c',
-  'text/x-c++src',
-  'text/x-chdr',
-  'text/x-c++hdr',
-  'text/x-java-source',
-  'text/x-csharp',
-  'text/x-go',
-  'text/x-rustsrc',
-  'application/x-ruby',
-  'application/x-httpd-php',
-  'text/x-swift',
-  'text/x-kotlin',
-  'text/x-scala',
-  'application/x-sh',
-  'text/x-script.zsh',
-  'text/html',
-  'text/css',
-  'application/json',
-  'application/xml',
-  'application/x-yaml',
-  'application/sql',
-  'text/markdown',
+  'application/vnd.ms-powerpoint',
 ]
 
 export function isPreviewable(type) {
-  console.log('COMPILED')
   return previewableMimeTypes.includes(type)
 }
